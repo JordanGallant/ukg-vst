63b0631e65447b3fd5a6446f45cdaa7bace9db31
Jordan <39-12602@sae.edu.za>
2025-05-08 11:36:33 +0200

    remove build

--- build/CMakeFiles/MyPlugin.dir/Users/jordan/Desktop/2025/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MyPlugin.dir/Users/jordan/Desktop/2025/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o: \
-  /Users/jordan/Desktop/2025/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp
--- build/CMakeFiles/MyPlugin.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "MyPlugin_artefacts/libUKG_SharedCode.a"
-)
